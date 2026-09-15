1618f89b72315648812c6b3f515c05d4305915df
AmlDev887 <xolmurodovamalbek494@gmail.com>
2026-09-15 14:46:09 +0500

    Add href

--- app/parsing.py
+++ app/parsing.py
@@ -5,12 +5,21 @@ from pydantic import BaseModel,TypeAdapter
 from bs4 import BeautifulSoup
 
 session = requests.Session()
-url = "https://habr.com/ru/articles/803869/"
-file = "robots.txt"
+base_url = "https://quotes.toscrape.com"
+url = "https://quotes.toscrape.com/"
+file = "robots.json"
+while True:
+    
+    response = requests.get(url)
+    html = response.text
 
-                    
-response = requests.get(url)
-html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    with open(file,"w",encoding="utf-8") as f:
+        json.dump(soup.get_text(), f, indent=4, ensure_ascii=False)
 
-soup = BeautifulSoup(html, "html.parser")
-text = soup.get_text() 
+    next_button = soup.find("li",class_="next")
+    if next_button == None:
+        print("Процесс окончен!")
+        break
+    next_url = next_button.find("a")["href"]
+    url = base_url + next_url
